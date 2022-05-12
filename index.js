ffdf178186ab869bdb638d21e195681358efd407
sadio <jwjeong6542@gmail.com>
2022-05-12 22:50:56 +0900

    회원가입 추가

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require("express"); // express 모듈 가져오기
 const app = express();
 const port = 5000;
+const bodyParser = require("body-parser");
+const { User } = require("./models/User"); // 유저 가져오기
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// application/json
+app.use(bodyParser.json());
 
 const mongoose = require("mongoose");
 mongoose
@@ -14,4 +22,16 @@ mongoose
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => res.send("Hello World!"));
+
+app.post("/register", (req, res) => {
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      succes: true,
+    });
+  });
+});
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
