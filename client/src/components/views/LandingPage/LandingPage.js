0094bfe2d03a03d2ba980bc986704432d33e43e5
sadio <jwjeong6542@gmail.com>
2022-06-13 20:43:58 +0900

    20220613

---
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -0,0 +1,14 @@
+import React, { useEffect } from "react";
+import axios from "axios";
+
+function LandingPage() {
+  useEffect(() => {
+    axios.get("/api/hello").then((res) => {
+      console.log(res.data);
+    });
+  }, []);
+
+  return <div>LandingPage1</div>;
+}
+
+export default LandingPage;
